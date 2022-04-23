a9c67ad4fa4b1ced960c654edf7161a7b92f286c
blackmitnick <mkakarianderson68@gmail.com>
2022-04-24 00:58:00 +0300

    custom script for converting html to pdf

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 "use strict";
-
+const element = document.querySelector("#element-to-print");
 const btn= document.querySelector(".btn");
 btn.addEventListener('click',elementToPrint());
 
 const elementToPrint = ()=>{
-
+html2pdf().from(element).save();
 }
